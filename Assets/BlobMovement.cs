0cb7c8fd425045774b786eef9c65b60e83b4a1fa
Jacob Hagstedt <jacob.hagstedt@gmail.com>
2020-01-02 20:54:07 +0100

    Add movement script and apply to sample scene

---
+++ Assets/BlobMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D blob;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        blob.MovePosition(blob.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
